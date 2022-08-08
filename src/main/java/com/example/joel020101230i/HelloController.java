7655bdad7fa926cc7e588aceb8473df34a62decd
Jhoel cr <jcusironco7@hotmail.com>
2022-08-08 13:53:26 -0500

    Initial commit

---
+++ src/main/java/com/example/joel020101230i/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.joel020101230i;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.joel020101230i {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.joel020101230i to javafx.fxml;
+    exports com.example.joel020101230i;
+}
